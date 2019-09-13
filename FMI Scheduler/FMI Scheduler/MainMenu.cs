6d140fdadc5f654663eee35394ccf8a1b7fb6e51
ademastrie <ademastrie@gmail.com>
2019-09-13 11:50:33 -0400

    Big update adding the functionality to select multiple items in the order list and then sort them by material size and punches. New class that handles this new functionality. Will also print to stock files to run on FMI

---
+++ FMI Scheduler/FMI Scheduler/MainMenu.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+using BrightIdeasSoftware;
+
+namespace FMI_Scheduler
+{
+    public partial class MainMenu : Form
+    {
+        StockOptimizer stockOptimizer = new StockOptimizer();
+
+
+        String[] split; //TODO make a more sensible naming convention -- This stores the splits for the part files line by line
+        String[] fileSplit; //TODO make a more sensible naming convention -- This stores the file name splits for order # and left/right
+        StreamReader sr; //TODO Programatically aquire all part files in a directory
+        string filePath = null; // saves the name of the part file to later split to get order and left/right info
+        string filePathName = null;
+        string line; //stores the lines from part files
+        string dashNum;
+        Dictionary<string, string> myVars; // stores all of the splits from a part file
+        
+        //TODO Will want to create these directories programatically or even allow for them to be set in options
+        string partsDirectory = @"../../../Parts";
+        string processedDirectory = @"../../../Parts\Processed\";
+        string errorDirectory = @"../../../Parts\Error\";
+        string completedDirectory = @"../../../Parts\Completed\";
+
+
+        public MainMenu()
+        {
+
+            Directory.CreateDirectory(partsDirectory);
+            Directory.CreateDirectory(processedDirectory);
+            Directory.CreateDirectory(errorDirectory);
+            Directory.CreateDirectory(completedDirectory);
+            Directory.CreateDirectory(@"../../../obj\");
+
+            InitializeComponent();
+            ReadFile();
+            ActiveObjList();
+            
+        }
+
+
+        [Serializable()]
+        public class SalesOrder
+                {
+            public string FilePath { get; set; }
+            public string Order { get; set; }
+            public string WallMidOutside { get; set; }                
+            public string LeftRight { get; set; }
+            public float Length { get; set; }
+            public int QTY { get; set; }
+            public string Material { get; set; }
+            public string TZ1 { get; set; }
+            public string TZ2 { get; set; }
+            public string TZ3 { get; set; }
+            public string TY1 { get; set; }
+            public string TY2 { get; set; }
+            public string TY3 { get; set; }
+            public string UploadedTime { get; set; }
+            public bool isCompleted { get; set; }
+                    
+                }
+     
+        /*ReadFile will iterate through all part files splitting the lines at the = and storing the two halfs of the line is the dictionary myVars. 
+         * For every file an object will be created to store the information for later use */
+        private void ReadFile()
+        {
+            var prtFiles = Directory.EnumerateFiles(partsDirectory, "*.prt");
+            if (prtFiles != null)
+            {
+                foreach (string currentFile in prtFiles)
+                {
+
+                        sr = new StreamReader(currentFile);
+                        if (sr.BaseStream is FileStream)
+                        {
+                            filePath = (sr.BaseStream as FileStream).Name;
+                            filePathName = Path.GetFileName(filePath);
+                            fileSplit = filePathName.Split(new char[] { '-', '.' });
+                        }
+
+                        myVars = new Dictionary<string, string>();
+                        while ((line = sr.ReadLine()) != null && line.Contains('='))
+                        {
+                            split = line.Split(new char[] { '=' });
+                            myVars.Add(split[0], split[1]);
+                        }
+                        sr.Close();
+                        PopulateSalesOrder();
+                    
+                }
+            }
+            
+        }
+        //TODO need error checking to make sure the .prt file is formatted correctly.
+        //create our sale order objects and serialize them so that we dont have to do it everytime the program runs
+        private void PopulateSalesOrder()
+        {
+            try
+            {
+                string serObjectPath = @"../../../obj\" + fileSplit[1] + fileSplit[2] + ".bin"; //path to save a binary file containing object details
+
+                if (!File.Exists(serObjectPath))//if we already created this object dont do it again
+                {
+
+                    //Split the last part of the part file to determine the dash number of the sales order.
+                    string[] lastSplit = fileSplit[2].Split(new char[] { 'W', 'M', 'O' });
+                    dashNum = lastSplit[1][0].ToString();
+
+                    SalesOrder salesOrder = new SalesOrder
+                    {
+                        FilePath = filePath,
+                        Order = fileSplit[1] + "-" + dashNum,
+                        Length = float.Parse(myVars["LEN "]),
+                        QTY = int.Parse(myVars["QTY "]),
+                        Material = myVars["SEC "],
+                        UploadedTime = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"),
+                        isCompleted = false
+                    };
+
+                    //TODO make a ternary operator for consistency
+                    //if we have a tool declared in the part file assign it to the saleOrder variable if not make it null.
+                    string result = null;
+                    if (myVars.TryGetValue("TZ1 ", out result)) { salesOrder.TZ1 = result; } else salesOrder.TZ1 = null;
+                    if (myVars.TryGetValue("TZ2 ", out result)) { salesOrder.TZ2 = result; } else salesOrder.TZ2 = null;
+                    if (myVars.TryGetValue("TZ3 ", out result)) { salesOrder.TZ3 = result; } else salesOrder.TZ3 = null;
+                    if (myVars.TryGetValue("TY1 ", out result)) { salesOrder.TY1 = result; } else salesOrder.TY1 = null;
+                    if (myVars.TryGetValue("TY2 ", out result)) { salesOrder.TY2 = result; } else salesOrder.TY2 = null;
+                    if (myVars.TryGetValue("TY3 ", out result)) { salesOrder.TY3 = result; } else salesOrder.TY3 = null;
+
+
+                    if (fileSplit[2].Contains("W")) { salesOrder.WallMidOutside = "Wall"; }
+                    if (fileSplit[2].Contains("M")) { salesOrder.WallMidOutside = "Middle"; }
+                    if (fileSplit[2].Contains("O")) { salesOrder.WallMidOutside = "Outside"; }
+
+                    //if there is more than one character in lastSplit[1] we can determine that it is a right hand file.
+                    salesOrder.LeftRight = (lastSplit[1].Length > 1) ? "Right" : "Left";
+
+
+
+                    //move the file from the root directory to a folder so we dont have to keep looping through the parts(this may have undesired effets)
+                    File.Move(salesOrder.FilePath, processedDirectory + filePathName);
+                    salesOrder.FilePath = processedDirectory + filePathName;//set the filepath to the new location
+
+                    Stream saveFileStream = File.Create(serObjectPath);
+                    BinaryFormatter serializer = new BinaryFormatter();
+                    serializer.Serialize(saveFileStream, salesOrder);
+                    saveFileStream.Close();
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("No such Key");
+                File.Move(filePath, errorDirectory + DateTime.Now.ToString("yyyyddMHHmmss") + "-" + filePathName);
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Index out of range");
+                File.Move(filePath, errorDirectory + DateTime.Now.ToString("yyyyddMHHmmss") + "-" + filePathName);
+            }
+
+            catch (IOException)
+            {
+                Console.WriteLine("File already exists in destination folder");
+                File.Move(filePath, errorDirectory + DateTime.Now.ToString("yyyyddMHHmmss") + "-" + filePathName);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong: " + e);
+                File.Move(filePath, errorDirectory + DateTime.Now.ToString("yyyyddMHHmmss") + "-" + filePathName);
+            }
+         }          
+        
+
+
+        private void ActiveObjList()
+        {
+            var binFiles = Directory.EnumerateFiles(@"../../../obj ", "*.bin");
+            if (binFiles != null)
+            {
+                foreach (string currentFile in binFiles)
+                {
+                    Stream openFileStream = File.OpenRead(currentFile);
+                    BinaryFormatter deserializer = new BinaryFormatter();
+                    SalesOrder salesOrder = (SalesOrder)deserializer.Deserialize(openFileStream);
+
+                    if (ActiveList.FindItemWithText(salesOrder.FilePath) == null)
+                    {
+                        ActiveList.AddObject(salesOrder);
+                    }
+                    openFileStream.Close();
+                }
+            }
+        }
+    
+        private void CompletedObjList()
+        {
+            var binFiles = Directory.EnumerateFiles(@"../../../obj ", "*.bin");
+            if (binFiles != null)
+            {
+                foreach (string currentFile in binFiles)
+                {
+                    Stream openFileStream = File.OpenRead(currentFile);
+                    BinaryFormatter deserializer = new BinaryFormatter();
+                    SalesOrder salesOrder = (SalesOrder)deserializer.Deserialize(openFileStream);
+
+
+                    if (CompletedList.FindItemWithText(salesOrder.FilePath) == null && salesOrder.isCompleted == true)
+                    {
+                        CompletedList.AddObject(salesOrder);
+                    }
+                    openFileStream.Close();
+                }
+            }
+        }
+
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            ReadFile();
+        }
+
+        private void PartListTimer_Tick(object sender, EventArgs e)
+        {
+            ActiveObjList();
+           // CreateActiveList();
+        }
+
+        private void Stock_MouseClick(object sender, MouseEventArgs e)
+        {
+            List<SalesOrder> rawOrderList = new List<SalesOrder>();
+            foreach (var selectedObject in ActiveList.SelectedObjects)
+            {
+                rawOrderList.Add((SalesOrder)selectedObject);
+            }
+            stockOptimizer.BuildStock(rawOrderList);
+        }
+    }
+}
--- FMI Scheduler/FMI Scheduler/StockOptimizer.cs
+++ FMI Scheduler/FMI Scheduler/StockOptimizer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,136 @@ namespace FMI_Scheduler
 {
     class StockOptimizer
     {
+        //for every order that was selected loop through each list to see if a stock list has already been made for that material and punch layout
+        //if not create one and add the item to it. 
+        public void BuildStock(List<MainMenu.SalesOrder> rawOrderList)
+        {
+            List<List<MainMenu.SalesOrder>> stockList = new List<List<MainMenu.SalesOrder>>();
+            
+            foreach (MainMenu.SalesOrder salesOrder in rawOrderList)
+            {
+                bool isCategoryfound = false;
+                
+                if (stockList.Count == 0)
+                {
+                    List<MainMenu.SalesOrder> sortedOrderList = new List<MainMenu.SalesOrder>();
+                    sortedOrderList.Add(salesOrder);
+                    stockList.Add(sortedOrderList);
+                    continue;
+                }
+
+                foreach (List<MainMenu.SalesOrder> orderList in stockList)
+                {
+                    List<MainMenu.SalesOrder> sortedOrderList = new List<MainMenu.SalesOrder>();
+                    foreach (MainMenu.SalesOrder order in orderList)
+                    {
+                        if (order.Material == salesOrder.Material
+                             && (order.TY1 == salesOrder.TY1 || order.TY1 == null)
+                             && (order.TY2 == salesOrder.TY2 || order.TY2 == null)
+                             && (order.TY3 == salesOrder.TY3 || order.TY3 == null)
+                             && (order.TZ1 == salesOrder.TZ1 || order.TZ1 == null)
+                             && (order.TZ2 == salesOrder.TZ2 || order.TZ2 == null)
+                             && (order.TZ3 == salesOrder.TZ3 || order.TZ3 == null))
+                        {
+                            isCategoryfound = true;
+                            orderList.Add(salesOrder);
+                            break;
+                        }
+                    }
+                    if (isCategoryfound == true)
+                    {
+                        break;
+                    }
+
+                }
+                if (isCategoryfound == false)
+                {
+                    List<MainMenu.SalesOrder> sortedOrderList = new List<MainMenu.SalesOrder>();
+                    sortedOrderList.Add(salesOrder);
+                    stockList.Add(sortedOrderList);
+                }
+
+            }
+            WriteToFile(stockList);
+        }
+
+
+        public void WriteToFile(List<List<MainMenu.SalesOrder>> stockList)
+        {
+            float totalLength = 0f;
+            int stockLength = 36000;
+            float cropCut = 0.5f;
+            int orderQTY = 0;
+            string bin = "0";//TODO give user the option to set this
+
+            string[] stockArray = new string[15];
+
+            foreach (List<MainMenu.SalesOrder> orderList in stockList)
+            {
+                List <List<String>> orderInfoList = new List<List<String>>();//list used to store all of the orders in the stock to print at the end of function
+                totalLength = 0f;
+                orderQTY = 0;
+                foreach (MainMenu.SalesOrder salesOrder in orderList)//get information from all of the orders stored in the order list
+                {
+                    string prtFileName = Path.GetFileName(salesOrder.FilePath);
+                    List<String> orderInfo = new List<string> { prtFileName, salesOrder.Length.ToString(), salesOrder.QTY.ToString(), bin , salesOrder.Order};
+                    orderInfoList.Add(orderInfo);
+                    totalLength += salesOrder.Length;
+                    orderQTY += 1;
+
+
+                }
+                //this group of functions will store information that is common between all of the sales orders. useful in case a Y punch is used on one angle and the Z is used on the mirror.
+                var material = orderList.Select(o => o.Material).FirstOrDefault().ToString().Trim();
+                String[] stockSplit = material.Split(new char[] { 'A', 'X' });
+
+
+
+                var TY1 = orderList.Select(o => o.TY1).DefaultIfEmpty("").FirstOrDefault();
+                var TY2 = orderList.Select(o => o.TY2).DefaultIfEmpty("").FirstOrDefault();
+                var TY3 = orderList.Select(o => o.TY3).DefaultIfEmpty("").FirstOrDefault();
+                var TZ1 = orderList.Select(o => o.TZ1).DefaultIfEmpty("").FirstOrDefault();
+                var TZ2 = orderList.Select(o => o.TZ2).DefaultIfEmpty("").FirstOrDefault();
+                var TZ3 = orderList.Select(o => o.TZ3).DefaultIfEmpty("").FirstOrDefault();
+                
+
+                //populate the array used to print lines to the stock files
+                stockArray[0] = material;
+                stockArray[1] = stockSplit[1];
+                stockArray[2] = stockSplit[2];
+                stockArray[3] = stockSplit[3];
+                stockArray[4] = (totalLength + cropCut).ToString();
+                stockArray[5] = stockLength.ToString();
+                stockArray[6] = cropCut.ToString();
+                stockArray[7] = "0";//not entirely sure if this has a real purpose. Always shows as zero in all the stock ive seen.
+                stockArray[8] = orderQTY.ToString();
+                stockArray[9] = (TY1 == null) ? "" : "TY1 = " + TY1.Trim();
+                stockArray[10] = (TY2 == null) ? "" : "TY2 = " + TY2.Trim();
+                stockArray[11] = (TY3 == null) ? "" : "TY3 = " + TY3.Trim();
+                stockArray[12] = (TZ1 == null) ? "" : "TZ1 = " + TZ1.Trim();
+                stockArray[13] = (TZ2 == null) ? "" : "TZ2 = " + TZ2.Trim();
+                stockArray[14] = (TZ3 == null) ? "" : "TZ3 = " + TZ3.Trim();
+
+                //TODO may need to reconsider how we name the file. If two files are made inthe same second with the same material they may override each other.
+                using (StreamWriter outputFile = new StreamWriter("../../../Stocks/"+ material + DateTime.Now.ToString("mms") + ".stk"))
+                {
+                    foreach (string line in stockArray)//print the stock info
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                    foreach(List<string> list in orderInfoList)//print order information after all of the general stock info
+                    {
+                        foreach(string line in list)
+                        {
+                            outputFile.WriteLine(line);
+                        }
+                    }
+                    outputFile.Close();
+                }
+            }
+
+        }
+
+
     }
 }
